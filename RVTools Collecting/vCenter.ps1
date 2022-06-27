83a96987e74aad63e2fa80cb2e89c646413172a7
Lorenzo <sldgkithop@gmail.com>
2022-06-27 13:22:04 +0200

    Update vCenter.ps1

--- RVTools Collecting/vCenter.ps1
+++ RVTools Collecting/vCenter.ps1
@@ -6,7 +6,7 @@ $currentdate = (get-date -uformat %Y-%m-%d).tostring()
 $currentyearmonth = (get-date -uformat %Y-%m).tostring()
  
 # Creazione cartella di logging per il mese corrente e inizio log transcript.
-$Logs = "C:\...\Logs\$currentyearmonth"
+$Logs = "C:\...\vCenter\Logs\$currentyearmonth"
 If(!(test-path $Logs)) {
 	New-Item -ItemType Directory -Force -Path $Logs
 }
@@ -15,7 +15,7 @@ $logfile = "$Logs\${currentdate}_log_${Server}.txt"
 start-transcript -path $logfile
  
 # Crezione cartella di destinazione per i reports del mese corrente.
-$Reports = "C:\...\Reports\$currentyearmonth"
+$Reports = "C:\...\vCenter\Reports\$currentyearmonth"
 If(!(test-path $Reports)) {
 	New-Item -ItemType Directory -Force -Path $Reports
 }
@@ -34,4 +34,24 @@ if($Process.ExitCode -eq -1) {
  
 Write-Host
 Write-Host "Export concluded."
+Write-Host
+Write-Host "Compressing reports..."
+
+# Definizione parametri.
+$currentdate = (get-date -uformat %Y-%m-%d).tostring()
+$currentyearmonth = (get-date -uformat %Y-%m).tostring()
+$Reports = "C:\...\Reports\$currentyearmonth"
+ 
+# Compressione in .zip ed eliminazione dei file .xlsx.
+Get-ChildItem $Reports\$currentdate* | Compress-Archive -DestinationPath "$Reports\${currentdate}_RVTools.zip" -CompressionLevel Optimal
+Start-Sleep -s 5
+Get-ChildItem –Path $Reports -filter "${currentdate}*.xlsx" | Remove-Item
+ 
+# Copia su path Dev per elaborazioni.
+Copy-Item "$Reports\${currentdate}_RVTools.zip" -Destination "C:\...\Dev\vCenter" -Recurse
+
+
+Write-Host
+Write-Host "Reports compressed."
+Write-Host
 Stop-Transcript
